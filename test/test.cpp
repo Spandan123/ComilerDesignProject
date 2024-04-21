de5af765390a68d7adcd975420890547ae052ef8
unknown <spandanbhs@gmail.com>
2024-04-22 01:00:28 +0530

    some changes

--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,7 @@
 #include "lexer/token_stream.h"
 #include "parser/parser.h"
 #include "parser/LR1_automaton.h"
+#include "parser/parsing_table.h"
 int main(int argc, char *argv[])
 {
     char *ffp = "test.txt";
@@ -14,9 +15,22 @@ int main(int argc, char *argv[])
         exit(1);
     }
     FILE *new_fp = fopen("generated_token.txt", "w");
-    fprintf(new_fp, "%s", token_stream(fp));
+    char *tokens = token_stream(fp);
+    fprintf(new_fp, "%s", tokens);
     fclose(fp);
     fclose(new_fp);
+    run_parser();
+    LR1_item_set::create_LR1_automaton();
+    print_automata();
+    create_reduces();
+    print_item_mapping();
+    // print_reduces();
+    create_parsing_table();
+    print_parsing_table();
+    if (parse(tokens))
+    {
+        cout << "\n\nPARSING SUCCESSFUL!\n";
+    }
     remove(argv[0]);
     return 0;
 }
